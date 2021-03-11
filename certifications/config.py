dc619a3acbf5de901102657dbcc5a442bf5b064d
Yam Mesicka <yammesicka@gmail.com>
2021-03-12 01:31:04 +0200

    feat: Create working Flask website with syllabus (draft)

---
+++ certifications/config.py
@@ -0,0 +1,7 @@
+import pathlib
+
+
+CWD = pathlib.Path(__file__).parent
+DB_PATH = CWD / 'resources' / 'students.db'
+JSON_PATH = CWD / 'resources' / 'students.json'
+SYLLABUS_PATH = CWD / 'resources' / 'syllabus.json'
